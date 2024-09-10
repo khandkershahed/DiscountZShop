722cb80ae6594e0aacc253bbdec8c8fd164b57b2
shahed141123 <khandkershahed23@gmail.com>
2024-09-10 17:59:27 +0600

    31-08-24

--- app/Models/OfferType.php
+++ app/Models/OfferType.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use App\Traits\HasSlug;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class OfferType extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug;
+
+    protected $slugSourceColumn = 'name';
 
     /**
      * The attributes that aren't mass assignable.
