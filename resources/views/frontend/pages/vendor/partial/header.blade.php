c5a282c404f174856c2b05188d12ba5b95bd73dd
shahed141123 <khandkershahed23@gmail.com>
2024-11-11 18:08:45 +0600

    first commit

--- resources/views/frontend/pages/vendor/partial/header.blade.php
+++ resources/views/frontend/pages/vendor/partial/header.blade.php
@@ -40,11 +40,13 @@
                                 </div>
                             </a>
                         @endif
-                        <a href="{{ route('vendor.offers', optional($brand)->slug) }}">
-                            <div class="vendor-menus {{ Route::is('vendor.offers') ? 'active' : '' }} rounded-2">
-                                <h6>Offers</h6>
-                            </div>
-                        </a>
+                        @if (optional($brand)->offers->count() > 0)
+                            <a href="{{ route('vendor.offers', optional($brand)->slug) }}">
+                                <div class="vendor-menus {{ Route::is('vendor.offers') ? 'active' : '' }} rounded-2">
+                                    <h6>Offers</h6>
+                                </div>
+                            </a>
+                        @endif
 
                     </div>
                 </div>
