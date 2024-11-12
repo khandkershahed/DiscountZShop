bc34f3a0d7f0c0b09707d53b48c73aee3d2e8a79
shahed141123 <khandkershahed23@gmail.com>
2024-11-12 15:41:38 +0600

    first commit

--- resources/views/components/metronic/input.blade.php
+++ resources/views/components/metronic/input.blade.php
@@ -18,10 +18,9 @@
     </div>
 @enderror --}}
 
-<input class="form-control @error($name)is-invalid @enderror" id="{{ $id ?? '' }}"
-    type="{{ $type ?? 'text' }}" name="{{ $name }}" step="0.01"
-    placeholder="{{ $placeholder ?? 'Complete the field' }}" maxlength="250" value="{{ old($name, $value ?? '') }}"
-    aria-label="{{ $placeholder ?? 'input' }} example" {{ $required ?? '' }}>
+<input class="form-control @error($name)is-invalid @enderror" id="{{ $id ?? '' }}" type="{{ $type ?? 'text' }}"
+    name="{{ $name }}" step="0.01" placeholder="{{ $placeholder ?? 'Complete the field' }}" maxlength="250"
+    value="{{ old($name, $value ?? '') }}" aria-label="{{ $placeholder ?? 'input' }} example" {{ $required ?? '' }}>
 
 @error($name)
     <div class="invalid-feedback">
@@ -29,5 +28,14 @@
     </div>
 @enderror
 
+{{-- <input id="{{ $id ?? '' }}" type="{{ $type ?? 'text' }}"
+    class="form-control form-control-solid @error($name)is-invalid @enderror" name="{{ $name }}" step="0.01"
+    maxlength="250" placeholder="{{ $placeholder ?? '' }}" value="{{ old($name, $value ?? '') }}"
+    {{ $required ?? '' }} />
+@error($name)
+    <div class="invalid-feedback">
+        {{ $message }}
+    </div>
+@enderror --}}
 {{-- <x-input id="full_name" type="text" name="full_name" placeholder="Enter full name"
     colSize="col-lg-8"></x-input> --}}
