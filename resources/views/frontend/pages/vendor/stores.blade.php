c63dbda3137f7f82d3fde0b1443c3f8a992a898c
shahed141123 <khandkershahed23@gmail.com>
2024-11-12 12:54:14 +0600

    first commit

--- resources/views/frontend/pages/vendor/stores.blade.php
+++ resources/views/frontend/pages/vendor/stores.blade.php
@@ -29,10 +29,13 @@
                     </div>
                 </div>
             </div>
+            @php
+                $stores = $brand->stores ?? null;
+            @endphp
             <div class="row mt-4">
                 <div class="col-lg-5">
                     <ul class="nav nav-tabs border-0" id="myTab" role="tablist">
-                        @foreach (optional($brand)->stores as $store)
+                        @foreach ($stores as $store)
                             <li class="nav-item w-100 vendor-store-items mb-2" role="presentation">
                                 <button
                                     class="nav-link w-100 vendor-store {{ $loop->first ? 'active' : '' }} border-0 text-start"
@@ -56,28 +59,27 @@
                             </li>
                         @endforeach
                     </ul>
-
                 </div>
+
                 <div class="col-lg-7">
                     <div class="tab-content">
-                        @foreach (optional($brand)->stores as $store)
+                        @foreach ($stores as $store)
                             <div class="tab-pane fade {{ $loop->first ? 'show active' : '' }}"
-                                 id="home-{{ optional($store)->id }}" role="tabpanel"
-                                 aria-labelledby="home-{{ optional($store)->id }}-tab">
+                                id="home-{{ optional($store)->id }}" role="tabpanel"
+                                aria-labelledby="home-{{ optional($store)->id }}-tab">
                                 <div class="card rounded-0 shadow-sm border">
                                     <div class="card-body p-2">
                                         <iframe src="{{ optional($store)->url }}" width="100%" height="400"
-                                                frameborder="0" style="border: 0" allowfullscreen="" loading="lazy"
-                                                referrerpolicy="no-referrer-when-downgrade" class="map-store"></iframe>
+                                            frameborder="0" style="border: 0" allowfullscreen="" loading="lazy"
+                                            referrerpolicy="no-referrer-when-downgrade" class="map-store"></iframe>
                                     </div>
                                 </div>
                             </div>
                         @endforeach
                     </div>
-
-
                 </div>
             </div>
+
         </div>
     </section>
 </x-frontend-app-layout>
