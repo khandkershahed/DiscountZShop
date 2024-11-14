5417d8b19a0735bc19e04061a76a9caff9835c81
shahed141123 <khandkershahed23@gmail.com>
2024-11-14 09:39:18 +0600

    first commit

--- resources/views/frontend/pages/store_division_search.blade.php
+++ resources/views/frontend/pages/store_division_search.blade.php
@@ -1,58 +1,57 @@
-@forelse ($latest_stores as $store)
-    <div class="col-lg-3">
-        <div class="card border-0 shadow-sm">
-            <div class="card-body p-0 rounded-2">
-                <!-- Store Banner -->
-                <div class="main-store-banner">
-                    <img class="img-fluid rounded-2"
-                        src="{{ !empty($store->image) ? url('storage/' . $store->image) : 'https://ui-avatars.com/api/?name=' . urlencode($store->name) }}"
-                        alt="arong-banner.jpg" />
-                </div>
-                <!-- Store Logo And Rating -->
-                <div class="px-3 pb-3">
-                    <div class="d-flex justify-content-between align-items-center">
-
-                        <div class="store-logo">
-                            <img class="img-fluid"
-                                src="{{ !empty($store->logo) ? url('storage/' . $store->logo) : 'https://ui-avatars.com/api/?name=' . urlencode($store->name) }}"
-                                alt="arong-logo.png" />
-                        </div>
-
-                        <div class="store-rating">
-                            <a href="{{ route('store.details', $store->slug) }}"
-                                class="btn btn-common-one rounded-circle store-btn"><i class="fa-solid fa-store"
-                                    aria-hidden="true"></i>
-                            </a>
-                        </div>
-
+@forelse ($latest_stores as $store_division)
+<div class="col-lg-3">
+    <div class="card border-0 shadow-sm mb-4">
+        <div class="card-body p-0 rounded-2">
+            <!-- Store Banner -->
+            <div class="main-store-banner">
+                <img class="img-fluid rounded-2"
+                    src="{{ !empty(optional($store_division->brand)->image) ? url('storage/' . optional($store_division->brand)->image) : asset('images/no-image(random).png') }}"
+                    alt="arong-banner.jpg" />
+            </div>
+            <!-- Store Logo And Rating -->
+            <div class="px-3 pb-3">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="store-logo">
+                        <img class="img-fluid"
+                            src="{{ !empty(optional($store_division->brand)->logo) ? url('storage/' . optional($store_division->brand)->logo) : asset('images/no-image(random).png') }}"
+                            alt="arong-logo.png" />
                     </div>
-                    <!-- Store Info -->
-                    <div class="pt-4 d-flex justify-content-between">
-                        <a href="{{ route('store.details', $store->slug) }}">
-                            <div>
-                                <h5>{{ $store->name }}</h5>
-                                <p>{{ $store->headquarter }}</p>
-                            </div>
+                    <div class="store-rating">
+                        <a href="{{ route('vendor.stores', optional($store_division->brand)->slug) }}"
+                            class="btn btn-common-one rounded-circle store-btn"><i
+                                class="fa-solid fa-store" aria-hidden="true"></i>
                         </a>
-                        <div>
-                            <button class="btn border-0 p-0 m-0 pe-2">
-                                <i class="fa-solid fa-location-dot ps-1 main-color fs-3" title="Store Location"
-                                    data-bs-toggle="modal" data-bs-target="#store-Location" aria-hidden="true"></i>
-                            </button>
-                        </div>
                     </div>
-                    <div class="bg-light mt-3 d-flex justify-content-between mb-3 p-2 rounded-3">
-                        <span class="bg-danger badge fw-normal">
-                            <i class="fa-solid fa-percent pe-2"></i>OFFER
-                        </span>
-                        <small class="text-sm">
-                            Get UpTo <span class="main-color">{{ $store->badge }}</span> Off
-                        </small>
+
+                </div>
+                <!-- Store Info -->
+                <div class="pt-4 d-flex justify-content-between store_title">
+                    <a href="{{ route('vendor.stores', optional($store_division->brand)->slug) }}">
+                        <div>
+                            <h5 style="width: 98%;margin-bottom: 0.75rem;">{{ $store_division->title }}</h5>
+                            <p>{{ $store_division->headquarter }}</p>
+                        </div>
+                    </a>
+                    <div>
+                        <button class="btn border-0 p-0 m-0 ps-2">
+                            <i class="fa-solid fa-location-dot ps-1 main-color fs-3"
+                                title="Store Location" data-bs-toggle="modal"
+                                data-bs-target="#store-Location" aria-hidden="true"></i>
+                        </button>
                     </div>
                 </div>
+                {{-- <div class="bg-light mt-3 d-flex justify-content-between mb-3 p-2 rounded-3">
+                    <span class="bg-danger badge fw-normal">
+                        <i class="fa-solid fa-percent pe-2"></i>OFFER
+                    </span>
+                    <small class="text-sm">
+                        Get UpTo <span class="main-color">{{ $store_division->badge }}</span> Off
+                    </small>
+                </div> --}}
             </div>
         </div>
     </div>
+</div>
 
 
 @empty
