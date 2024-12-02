7f5c2eb01870f755334cc54747fe4934d0217bf7
Sazeduzzaman <sazeduzzamansaju@gmail.com>
2024-12-02 10:12:09 +0600

    some changes done

--- resources/views/frontend/pages/vendor/offers.blade.php
+++ resources/views/frontend/pages/vendor/offers.blade.php
@@ -46,8 +46,8 @@
                                 <div class="col-lg-6">
                                     @if (!empty($offer->badge))
                                         {{-- <span>Upto</span> --}}
-                                        <h3 class="main-color special-font-box text-end">{{ $offer->badge }}
-                                        </h3>
+                                        <h4 class="main-color special-font-box text-end">{{ $offer->badge }}
+                                        </h4>
                                     @endif
                                 </div>
                                 <div class="col-lg-12 pt-4 offer_title">
