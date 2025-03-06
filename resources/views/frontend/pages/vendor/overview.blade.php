9c1c30b458eba242085dfd5b12133819a064e510
NgenItdev2 <dev2.ngenit@gmail.com>
2025-03-06 14:08:11 +0600

    Update By Ashiquzzaman office 06-03-2025

--- resources/views/frontend/pages/vendor/overview.blade.php
+++ resources/views/frontend/pages/vendor/overview.blade.php
@@ -24,60 +24,67 @@
         <section class="responsive-section">
             <div class="container">
                 <div class="pb-5 mb-5 row align-items-center">
+
                     <div class="col-lg-6">
+
                         <div>
                             <h5 class="">{{ optional($brand)->about_title }}</h5>
                             <p class="pt-3">
                                 {!! optional($brand)->about !!}
                             </p>
                         </div>
+
                     </div>
+
                     <div class="col-lg-6 col-6">
                         <div class="vendor-banners overlay-container">
                             <img class="img-fluid rounded-2 brand-imges"
                                 src="{{ !empty(optional($brand)->middle_banner_left) ? url('storage/' . optional($brand)->middle_banner_left) : asset('images/no-overview.jpg') }}"
-                                alt="" onerror="this.onerror=null; this.src='{{ asset('images/no-overview.jpg') }}';">
+                                alt=""
+                                onerror="this.onerror=null; this.src='{{ asset('images/no-overview.jpg') }}';">
                             <div class="overlay overlay-1"></div>
                             <div class="overlay overlay-2"></div>
                             <div class="overlay overlay-3"></div>
                         </div>
                     </div>
                 </div>
-                @if (!empty(optional($brand)->description_title) || !empty(optional($brand)->description))
+
+                @if (!empty(optional($brand)->offer_description_title) || !empty(optional($brand)->offer_description))
                     <div class="mt-5 mb-4 row align-items-center">
                         <div class="col-lg-6 col-6">
                             <div class="vendor-banners overlay-container">
                                 <img class="pt-4 img-fluid rounded-2 brand-imges pt-lg-0"
                                     src="{{ !empty(optional($brand)->middle_banner_right) && file_exists(public_path('storage/' . optional($brand)->middle_banner_right)) ? url('storage/' . optional($brand)->middle_banner_right) : asset('images/no-overview.jpg') }}"
-                                    alt="" onerror="this.onerror=null; this.src='{{ asset('images/no-overview.jpg') }}';">
+                                    alt=""
+                                    onerror="this.onerror=null; this.src='{{ asset('images/no-overview.jpg') }}';">
                                 <div class="overlay overlay-1"></div>
                                 <div class="overlay overlay-2"></div>
                                 <div class="overlay overlay-3"></div>
                             </div>
                         </div>
-                        {{-- <div class="col-lg-6">
-                            <h5 class="pb-4">{{ optional($brand)->description_title}}</h5>
-                            <p style="text-align: justify;">
-                                {!! optional($brand)->description !!}
-                            </p>
-                        </div> --}}
                         <div class="col-lg-6">
-                            <h5 class="pb-3">{{ optional($brand)->offer_description_title }}</h5>
+                            <h5 class="pb-4">{{ optional($brand)->offer_description_title }}</h5>
                             <p style="text-align: justify;">
                                 {!! optional($brand)->offer_description !!}
                             </p>
                         </div>
+                        {{-- <div class="col-lg-6">
+                            <h5 class="pb-3">{{ optional($brand)->offer_description_title }}</h5>
+                            <p style="text-align: justify;">
+                                {!! optional($brand)->offer_description !!}
+                            </p>
+                        </div> --}}
                     </div>
                 @endif
 
-                @if (!empty(optional($brand)->offer_description_title) || !empty(optional($brand)->offer_description))
+                @if (!empty(optional($brand)->description_title) || !empty(optional($brand)->description))
                     <div class="mb-5 row">
                         <div class="col-lg-12">
-                            {{-- <h5 class="pb-3">{{ optional($brand)->offer_description_title }}</h5>
+                            <h5 class="pb-3">{{ optional($brand)->description_title }}</h5>
                             <p style="text-align: justify;">
-                                {!! optional($brand)->offer_description !!}
-                            </p> --}}
-                            <h5 class="pb-4">{{ optional($brand)->description_title}}</h5>
+                                {!! optional($brand)->description !!}
+                            </p>
+                            <h5 class="pb-4">{{ optional($brand)->description_title }}</h5>
                             <p style="text-align: justify;">
                                 {!! optional($brand)->description !!}
                             </p>
@@ -90,70 +97,11 @@
                         </div>
                     </div>
                 @endif
+
             </div>
         </section>
         {{-- Overview End --}}
     </div>
 
-    {{-- For Mobile Only --}}
-    {{-- <div class="mobile-homepage" style="margin-bottom: 8rem">
-
-        <section>
-            <div class="container">
-                <div class="mb-4 row">
-                    <div class="col-lg-12">
-                        <div>
-                            <h3 class="fw-normal" style="font-size: 20px !important;">
-                                {{ optional($brand)->about_title }}</h3>
-                            <p class="pt-3">
-                                {!! optional($brand)->about !!}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="my-5 row">
-                    <div class="col-6">
-                        <div class="vendor-banners">
-                            <img class="img-fluid"
-                                src="{{ !empty(optional($brand)->middle_banner_left) ? url('storage/' . optional($brand)->middle_banner_left) : asset('images/no-overview-1.png') }}"
-                                alt="">
-                        </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="vendor-banners">
-                            <img class="img-fluid"
-                                src="{{ !empty(optional($brand)->middle_banner_right) && file_exists(public_path('storage/' . optional($brand)->middle_banner_right)) ? url('storage/' . optional($brand)->middle_banner_right) : asset('images/no-overview-2.png') }}"
-                                alt="">
-                        </div>
-                    </div>
-                </div>
-                @if (!empty(optional($brand)->description_title) || !empty(optional($brand)->description))
-                    <div class="mb-4 row">
-                        <div class="col-lg-12">
-                            <h2 class="pb-4" style="font-size: 20px !important;">
-                                {{ optional($brand)->description_title }}</h2>
-                            <p style="text-align: justify;">
-                                {!! optional($brand)->description !!}
-                            </p>
-                        </div>
-                    </div>
-                @endif
-
-                @if (!empty(optional($brand)->offer_description_title) || !empty(optional($brand)->offer_description))
-                    <div class="mb-4 row">
-                        <div class="col-lg-12">
-                            <h2 class="pb-4" style="font-size: 20px !important;">
-                                {{ optional($brand)->offer_description_title }}</h2>
-                            <p style="text-align: justify;">
-                                {!! optional($brand)->offer_description !!}
-                            </p>
-                        </div>
-                    </div>
-                @endif
-            </div>
-        </section>
-
-    </div> --}}
-    {{-- Overview End --}}
 
 </x-frontend-app-layout>
