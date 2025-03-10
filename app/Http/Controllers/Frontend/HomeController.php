4cc87b1f30324d0fb986d279d4d22430ef837887
shahed141123 <khandkershahed23@gmail.com>
2025-03-10 15:28:13 +0600

    26-01-25

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -48,13 +48,13 @@ class HomeController extends Controller
         $brandOffersRight = $brandOffers->skip(2);
 
         // Retrieve brands, sliders, banners, categories, and offer types in fewer queries
-        $brands        = Brand::latest()->get();
+        $brands        = Brand::select('id', 'slug', 'logo')->latest()->get();
         $sliders       = Slider::where('status', 'active')->latest('id')->get();
         $banner        = Banner::where('status', 'active')->latest('id')->first();
-        $coupons       = Coupon::select('id', 'badge', 'coupon_code')->latest()->get();
-        $offerTypes    = OfferType::latest()->get();
+        $coupons       = Coupon::latest()->get();
+        $offerTypes    = OfferType::latest()->limit(8)->get();
         $categories    = Category::latest()->limit(6)->get();
-        $offerCatTypes = OfferType::where('status', 'active')->latest()->limit(8)->get();
+        $offerCatTypes = $offerTypes;
 
         // Prepare the data array
         $data = [
