3a7cd70ce4363b1885bc38ced11609bb5ac4cbbc
shahed141123 <khandkershahed23@gmail.com>
2025-03-10 14:33:55 +0600

    26-01-25

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -72,7 +72,7 @@
 
     <!-- JavaScript Dependencies -->
     <script src="{{ asset('frontend/assets/js/bootstrap/bootstrap.bundle.min.js') }}"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    {{-- <script src="https://code.jquery.com/jquery-3.6.0.js"></script> --}}
     <script src="{{ asset('frontend/assets/js/icons/fontawesome.js') }}"></script>
     <script src="{{ asset('frontend/assets/js/script-dev.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
