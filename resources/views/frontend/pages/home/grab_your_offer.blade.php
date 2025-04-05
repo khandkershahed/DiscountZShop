6f3c8f9cb66b725ebe1312d9fd7ffd3a96eb33e3
shahed141123 <khandkershahed23@gmail.com>
2025-04-05 16:32:23 +0600

    05-04-25

--- resources/views/frontend/pages/home/grab_your_offer.blade.php
+++ resources/views/frontend/pages/home/grab_your_offer.blade.php
@@ -142,7 +142,7 @@
                                                                 <div class="col-4">
                                                                     <div class="grab-imgs">
                                                                         <img class="img-fluid rounded-2"
-                                                                            src="{{ !empty($alloffer->logo) ? url('storage/' . $alloffer->logo) : 'https://ui-avatars.com/api/?name=' . urlencode($alloffer->name) }}"
+                                                                            src="{{ !empty(optional($alloffer->brand)->logo) ? url('storage/' . optional($alloffer->brand)->logo) : 'https://ui-avatars.com/api/?name=' . urlencode(optional($alloffer->brand)->name) }}"
                                                                             alt="">
                                                                     </div>
                                                                 </div>
@@ -178,7 +178,7 @@
                                                                         <div class="col-4">
                                                                             <div class="grab-imgs">
                                                                                 <img class="img-fluid rounded-2"
-                                                                                    src="{{ !empty($category_offer->logo) ? url('storage/' . $category_offer->logo) : 'https://ui-avatars.com/api/?name=' . urlencode($category_offer->name) }}"
+                                                                                    src="{{ !empty(optional($category_offer->brand)->logo) ? url('storage/' . optional($category_offer->brand)->logo) : 'https://ui-avatars.com/api/?name=' . urlencode(optional($category_offer->brand)->name) }}"
                                                                                     alt="">
                                                                             </div>
                                                                         </div>
