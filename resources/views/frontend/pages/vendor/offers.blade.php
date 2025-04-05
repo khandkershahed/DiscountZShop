421758dac2ed482504eac0084ccd87faad3ebf5a
shahed141123 <khandkershahed23@gmail.com>
2025-04-05 16:21:12 +0600

    05-04-25

--- resources/views/frontend/pages/vendor/offers.blade.php
+++ resources/views/frontend/pages/vendor/offers.blade.php
@@ -33,56 +33,59 @@
                 </div>
                 <div class="row servicesContainer divisionContainer">
                     @foreach (optional($brand)->offers as $offer)
-                        <div class="mb-4 col-lg-4 col-6 pe-2">
-                            <a href="{{ route('offer.details', $offer->slug) }}">
-                                <div class="mb-4 border-0 shadow-sm card">
-                                    <div class="p-0 card-body rounded-2">
-                                        <!-- Store Banner -->
-                                        <div class="main-store-banner">
-                                            <img class="img-fluid rounded-2"
-                                                src="{{ !empty(optional($offer)->image) ? url('storage/' . optional($offer)->image) : asset('images/no-image(random).png') }}"
-                                                alt="" />
-                                        </div>
-                                        <!-- Store Logo And Rating -->
-                                        <div class="px-3 pb-0 pb-lg-0">
-                                            <div class="d-flex justify-content-between align-items-center">
-                                                <div class="store-logo">
-                                                    <img class="img-fluid"
-                                                        src="{{ !empty(optional($offer->brand)->logo) ? url('storage/' . optional($offer->brand)->logo) : asset('images/no-image(random).png') }}"
-                                                        alt="arong-logo.png"
-                                                        onerror="this.onerror=null; this.src='{{ asset('img/no-img.jpg') }}';" />
-                                                </div>
-                                                <div class="store-rating">
-                                                    <a href="{{ route('offer.details', $offer->slug) }}"
-                                                        class="btn btn-common-one rounded-circle store-btn"><i
-                                                            class="fa-solid fa-arrow-right fs-5" aria-hidden="true"></i>
-                                                    </a>
-                                                </div>
+                        {{-- @if ($offer->expiry_date >= Carbon\Carbon::now()->format('Y-m-d') || $offer->expiry_date == null) --}}
+                            <div class="mb-4 col-lg-3 col-6 pe-2">
+                                <a href="{{ route('offer.details', $offer->slug) }}">
+                                    <div class="mb-4 border-0 shadow-sm card">
+                                        <div class="p-0 card-body rounded-2">
+                                            <!-- Store Banner -->
+                                            <div class="main-store-banner">
+                                                <img class="img-fluid rounded-2"
+                                                    src="{{ !empty(optional($offer)->image) ? url('storage/' . optional($offer)->image) : asset('images/no-image(random).png') }}"
+                                                    alt="" />
                                             </div>
-                                            <!-- Store Info -->
-                                            <div class="d-flex justify-content-between store_title">
-                                                <a href="{{ route('offer.details', $offer->slug) }}">
-                                                    <div>
-                                                        <h6>
-                                                            {{ Str::words($offer->name, 5, '...') }}
-                                                        </h6>
+                                            <!-- Store Logo And Rating -->
+                                            <div class="px-3 pb-0 pb-lg-0">
+                                                <div class="d-flex justify-content-between align-items-center">
+                                                    <div class="store-logo">
+                                                        <img class="img-fluid"
+                                                            src="{{ !empty(optional($offer->brand)->logo) ? url('storage/' . optional($offer->brand)->logo) : asset('images/no-image(random).png') }}"
+                                                            alt="arong-logo.png"
+                                                            onerror="this.onerror=null; this.src='{{ asset('img/no-img.jpg') }}';" />
                                                     </div>
-                                                </a>
-                                                {{-- <div>
-                                                <div>
-                                                    <a href="{{ route('offer.details', $offer->slug) }}"
-                                                        class="p-0 m-0 border-0 btn ps-2">
-                                                        <i class="fa-solid fa-location-dot ps-1 main-color fs-3"
-                                                            title="Store Location"></i>
-                                                    </a>
+                                                    <div class="store-rating">
+                                                        <a href="{{ route('offer.details', $offer->slug) }}"
+                                                            class="btn btn-common-one rounded-circle store-btn"><i
+                                                                class="fa-solid fa-arrow-right fs-5" aria-hidden="true"></i>
+                                                        </a>
+                                                    </div>
                                                 </div>
-                                            </div> --}}
+                                                <!-- Store Info -->
+                                                <div class="d-flex justify-content-between store_title">
+                                                    <a href="{{ route('offer.details', $offer->slug) }}">
+                                                        <div>
+                                                            <h6>
+                                                                {{ Str::words($offer->name, 5, '...') }}
+                                                            </h6>
+                                                        </div>
+                                                    </a>
+                                                    {{-- <div>
+                                                    <div>
+                                                        <a href="{{ route('offer.details', $offer->slug) }}"
+                                                            class="p-0 m-0 border-0 btn ps-2">
+                                                            <i class="fa-solid fa-location-dot ps-1 main-color fs-3"
+                                                                title="Store Location"></i>
+                                                        </a>
+                                                    </div>
+                                                </div> --}}
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            </a>
-                        </div>
+                                </a>
+                            </div>
+
+                        {{-- @endif --}}
 
                         {{-- @if ($offer->expiry_date >= Carbon\Carbon::now()->format('Y-m-d'))
                             <div class="mb-4 col-lg-3">
@@ -211,7 +214,7 @@
                 </div>
                 <div class="row servicesContainer divisionContainer">
                     @foreach (optional($brand)->offers as $offer)
-                        @if ($offer->expiry_date >= Carbon\Carbon::now()->format('Y-m-d') || $offer->expiry_date == null)
+                        {{-- @if ($offer->expiry_date >= Carbon\Carbon::now()->format('Y-m-d') || $offer->expiry_date == null) --}}
                             <div class="mb-4 col-lg-3">
                                 <div class="border-0 shadow-sm card bg-light">
                                     <div class="p-4 row align-items-center">
@@ -258,7 +261,7 @@
                                     </div>
                                 </div>
                             </div>
-                        @endif
+                        {{-- @endif --}}
                     @endforeach
                 </div>
             </div>
